752b548efe979f99a8fd55ac259221958d9b8eb6
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-25 18:03:57 +0530

    Create hello.ts

---
+++ hello.ts
@@ -0,0 +1,15 @@
+let brocode:string = 'brocoHe';
+
+console.log(brocode.toUpperCase());
+
+export {} // To prevent error: Cannot redeclare block-scoped variable 'brocode'.ts(2451)
+
+let isgood:boolean = true;
+console.log(isgood);
+
+const none=()=>{
+    console.log('none');
+}
+none();
+
+
