24f285c4c292a6e7b025601df4b889b7b2fb6a73
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-26 11:33:14 +0530

    f

---
+++ src/obj.ts
@@ -0,0 +1,11 @@
+ const hitesh={
+    name:'Brocode',
+    age:20,
+    isActive:true
+ }
+
+ function createUser({name:string ,isPaid:boolean}):void{
+ console.log(name,isPaid);
+ }
+ createUser("hitesh",true);
+
---
+++ src/sarah.ts
@@ -0,0 +1,5 @@
+let sarah=(Sarah:string)=>{
+console.log(`${Sarah} is very good at yapping!`);
+
+}
+sarah("sarah_is_cutu");
